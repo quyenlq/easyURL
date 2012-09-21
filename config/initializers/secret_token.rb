2503ebe09dbd9750eee5e9866aa25ca610602977
Quyen LQ <lucquocquyen@gmail.com>
2012-09-21 08:15:57 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyURL::Application.config.secret_token = 'af9f01a68a7b364b702b5ac69d3dba24ef6416b3eb9eeb41f4c8f4ddd26b468672539ddebe6e2c6c6b212642be7b1ed432bf4d2b87ed8f773693566a8feae698'
